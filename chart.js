8f04c4524ffa18f0eeb9768cfd20aa6e5f60b118
Cosmin Potocean <cosmycx@gmail.com>
2018-02-12 16:36:41 -0500

    title space

--- chart.js
+++ chart.js
@@ -266,7 +266,7 @@ function makeChart (dataObj,  lku, compareStr, settingsObj, chartMountNodeIdStr)
 		      .attr('font-size', '14px')
           .attr('font-weight', 'bold')
 	        .attr('x', spaceLeftForText - paddingTextToChart + (svgChartWidth - spaceLeftForText) / 2)
-		      .attr('y', 20)
+		      .attr('y', 30)
 
 
 	let barTooltip = d3.select('#' + chartMountNodeIdStr)
